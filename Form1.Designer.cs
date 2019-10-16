453bc9091f72920b835314d846690ef398677620
bbayrakli <bayrakliburak@outlook.com>
2019-10-16 08:26:26 +0300

    login_Screen

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace LoginScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button button1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.UserName = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(54, 80);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "GİRİŞ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // UserName
+            // 
+            this.UserName.Location = new System.Drawing.Point(3, 5);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(169, 20);
+            this.UserName.TabIndex = 0;
+            this.UserName.Text = "Kullanıcı Adı";
+            this.UserName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(3, 31);
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '*';
+            this.Password.Size = new System.Drawing.Size(169, 20);
+            this.Password.TabIndex = 2;
+            this.Password.Text = "Parola";
+            this.Password.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.checkBox1.Location = new System.Drawing.Point(3, 57);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(97, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Parolayı Göster";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.UserName);
+            this.panel1.Controls.Add(this.Password);
+            this.panel1.Controls.Add(button1);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(37, 178);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(175, 128);
+            this.panel1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 149);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 318);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserName;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+namespace LoginScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        
+        public void check()
+        {
+
+            using (System.IO.StreamWriter file =
+                     new System.IO.StreamWriter(@"C:\Users\bbayrakli\Desktop\" + DateTime.UtcNow.ToString("yyyy_MM_dd") + "_" + UserName.Text + ".txt", true))
+
+                if (File.Exists(@"C:\Users\bbayrakli\Desktop\" + DateTime.UtcNow.ToString("yyyy_MM_dd") + "_" + UserName.Text + ".txt"))
+                {
+
+                    {
+                        file.WriteLine(DateTime.Now.ToString("hh:mm:ss")+" - Giriş Yapıldı");
+                    }
+                }
+                else
+                {
+
+                    System.IO.File.WriteAllText(@"C:\Users\bbayrakli\Desktop\" + DateTime.UtcNow.ToString("yyyy_MM_dd") + "_"
+                        + UserName.Text + ".txt", "Giriş Yapıldımı.\n");
+                }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.Activate();
+
+            
+
+                string strConn = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\Users.mdb";
+            using (OleDbConnection conn = new OleDbConnection(strConn))
+            {
+                conn.Open();
+                string select = "select * from Users where Username='" + UserName.Text + "'";
+                string find = "select * from Users where Password='" + Password.Text + "'";
+                using (OleDbCommand cmd = new OleDbCommand(select, conn))
+                    
+
+                {
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    if (count > 0)
+                    {
+                        using (OleDbCommand komut = new OleDbCommand(find, conn))
+
+                        {
+                            int say = Convert.ToInt32(komut.ExecuteScalar());
+                            if (say > 0)
+                            {
+                                OleDbDataReader reader = komut.ExecuteReader();
+                                MessageBox.Show("Giriş Başarılı");
+                                while (reader.Read())
+                                {
+                                    string UserName = reader["Ad_Soyad"].ToString();
+                                    label1.Text = UserName;
+                                }
+
+                                check(); 
+                                    
+                                    
+                                
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("Kullanıcı Adı veya Parola Hatalı.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Kullanıcı Adı veya Parola Hatalı");
+                    }
+                }
+            }
+            
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                Password.PasswordChar = '\0';
+            }
+            if (checkBox1.Checked == false)
+            {
+                Password.PasswordChar = '*';
+            }
+        }
+
+        public void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            System.Drawing.Graphics graphics = e.Graphics;
+            System.Drawing.Rectangle gradient_rectangle = new System.Drawing.Rectangle(0, 0, this.Width, this.Height);
+            System.Drawing.Drawing2D.LinearGradientBrush b = new System.Drawing.Drawing2D.LinearGradientBrush(gradient_rectangle, color1:Color.FromArgb(0,100,80), color2: Color.FromArgb(100,0,100), 225f);
+            graphics.FillRectangle(b, gradient_rectangle);
+        }
+
+
+        // WINDOW RESIZING WITH RATIO's
+       /* private void Form1_Resize(object sender, EventArgs e)
+        {
+            panel1.Location = new System.Drawing.Point((this.Width/2 - 175 / 2-11), (this.Height/2 - 149 / 2+74));
+            pictureBox1.Location = new System.Drawing.Point((this.Width / 2 - 228 / 2 - 9), (this.Height / 2 - 149 / 2 - 92)); 
+        }*/
+
+
+    }
+}
